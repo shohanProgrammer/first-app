8a555ce6dbb96173f6c6418cd92d463dd90510f5
shohanProgrammer <azizmahmud2014@gmail.com>
2023-03-05 15:34:11 +0600

    updating

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul className="Menu">
+          <li className="MenuItem">
+            <a href="https://react.org/">Pricing</a>
+          </li>
+          <li className="MenuItem">
+            <a href="https://react.org/">About</a>
+          </li>
+          <li className="MenuItem">
+            <a href="https://react.org/">Contact</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
---
+++ src/MyContent.js
@@ -0,0 +1,18 @@
+import "./MyContent.css";
+function MyContent() {
+  // eslint-disable-line no-unused-vars
+  return (
+    <div className="MainContainer">
+      <h1 className="header">My awesome website in React</h1>
+      <p>Reasons I love React</p>
+      <ol>
+        <li>It's composable</li>
+        <li>It's declarative</li>
+        <li>It's a hireable skill</li>
+        <li>It's actively maintained by skilled people</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyContent;
